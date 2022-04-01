ecb124c9d014f31312b026c14b764d5cd3ac022a
SangWook <imcool2551@gmail.com>
2022-04-01 23:58:08 +0900

    hello-jpa test

--- src/main/java/jpabasic/jpabasic/JpaMain.java
+++ src/main/java/jpabasic/jpabasic/JpaMain.java
@@ -2,7 +2,9 @@ package jpabasic.jpabasic;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -10,10 +12,29 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
+        try {
+            // code
 
+            List<Member> members = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(1)
+                    .getResultList();
 
-        em.close();
+            for (Member member : members) {
+                System.out.println(member);
+            }
+
+            // code
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
